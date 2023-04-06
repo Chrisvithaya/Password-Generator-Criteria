27eff58ddab53fe01cb78bdd0ab323dca76b81b4
Chris Vithaya <parker@Chriss-MacBook-Air.local>
2023-04-06 16:16:57 -0700

    copy files

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,82 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+var numbers = ['0','1','2','3','4','5','6','7','8','9'];
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+];
+function generatePassword(){
+var passwordLength = prompt("How long do you want your password to be?")
+
+var passwordNumber = parseInt(passwordLength)
+if(!passwordNumber){
+  return generatePassword()
+}
+
+var lowercase = confirm ("Would you like lowercase characters?")
+console.log("lowercase!", lowercase)
+
+
+
+
+
+return "Your password"
+
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
