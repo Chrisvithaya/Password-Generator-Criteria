8f6622abb8172dfcaa2732812e382f74efc0a58c
Chris Vithaya <parker@Chriss-MacBook-Air.local>
2023-04-06 17:32:33 -0700

    edit files

--- Develop/script.js
+++ Develop/script.js
@@ -62,22 +62,84 @@ var upperCasedCharacters = [
   'Y',
   'Z',
 ];
-function generatePassword(){
-var passwordLength = prompt("How long do you want your password to be?")
 
-var passwordNumber = parseInt(passwordLength)
-if(!passwordNumber){
-  return generatePassword()
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.',
+];
+
+function getRandomCharacter(array) {
+  var randomIndex = Math.floor(Math.random() * array.length);
+  var randomPasswordChar = array[randomIndex];
+  return randomPasswordChar;
 }
 
-var lowercase = confirm ("Would you like lowercase characters?")
-console.log("lowercase!", lowercase)
 
+function generatePassword(){
 
+var passwordLength = prompt("How long do you want your password to be?");
+var hasSpecialChars = confirm('Do you want special characters?');
+var hasUpperCase = confirm('Do you want uppercase letters?');
+var hasLowerCase = confirm('Do you want lowercase letters?');
+var hasNumbers = confirm('Do you want numbers');
 
+var passwordOptions = {
+  passwordLength: passwordLength,
+  hasSpecialChars: hasSpecialChars,
+  hasLowerCase: hasLowerCase,
+  hasUpperCase: hasUpperCase,
+  hasNumbers: hasNumbers,
+};
 
+var possiblePasswordChars = [];
+var password = [];
 
-return "Your password"
+if (passwordOptions.hasSpecialChars) {
+    possiblePasswordChars = possiblePasswordChars.concat(specialCharacters);
+    var randomChar = getRandomCharacter(specialCharacters);
+    password.push(randomChar);
+}
+if (passwordOptions.hasNumbers) {
+    possiblePasswordChars = possiblePasswordChars.concat(hasNumbers);
+    var randomChar = getRandomCharacter(hasNumbers);
+    password.push(randomChar);
+}
+if (passwordOptions.hasUpperCase) {
+    possiblePasswordChars = possiblePasswordChars.concat(hasUpperCase);
+    var randomChar = getRandomCharacter(hasUpperCase);
+    password.push(randomChar);
+}
+if (passwordOptions.hasLowerCase) {
+    possiblePasswordChars = possiblePasswordChars.concat(hasLowerCase);
+    var randomChar = getRandomCharacter(hasLowerCase);
+    password.push(randomChar);
+}
+
+for (var i = 0; i < passwordOptions.length - password.length; i++) {
+    var randomChar = getRandomCharacter(possiblePasswordChars);
+    password.push(randomChar);
+}
 
 }
 // Write password to the #password input
@@ -91,3 +153,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
